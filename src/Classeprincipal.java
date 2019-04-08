f6c5eaf78b514d9acbbc43a66eb8aa517611c895
gavogarvo <gustavo.silva1282@etec.sp.gov.br>
2019-04-08 20:19:40 -0300

    Upload dos arquivos

---
+++ src/Classeprincipal.java
@@ -0,0 +1,107 @@
+
+import java.util.Scanner;
+
+public class Classeprincipal {
+
+public static void main (String[] args){
+    Carro carro = new Carro();
+    
+    Scanner scan = new Scanner (System.in);
+    
+    System.out.print("Digite o nome do Carro:");
+    carro.nome = scan.nextLine();
+            
+    System.out.print("Digite a marca do Carro:");
+    carro.marca = scan.nextLine();
+    
+    System.out.print("Digite a cor do Carro:");
+    carro.cor = scan.nextLine();
+    
+    System.out.print("Digite o ano de fabricação do Carro:");
+    carro.fabricacao = scan.nextLine();
+    
+    System.out.print("Digite a largura do Carro:");
+    carro.largura = scan.nextLine();
+    
+    System.out.print("Digite o peso do Carro:");
+    carro.peso = scan.nextLine();
+    
+    System.out.println(
+    );
+    
+    System.out.println("Nome do Carro:" + carro.nome);
+    System.out.println("Marca do Carro:" + carro.marca);
+    System.out.println("Cor do Carro:" + carro.cor);
+    System.out.println("Fabricação do Carro:" + carro.fabricacao);
+    System.out.println("largura do Carro:" + carro.largura);
+    System.out.println("Peso do Carro:" + carro.peso);
+    
+    
+    System.out.println(
+    );
+    
+    Carro carro2 = new Carro();
+    
+    System.out.print("Digite o nome do Carro:");
+    carro2.nome = scan.nextLine();
+            
+    System.out.print("Digite a marca do Carro:");
+    carro2.marca = scan.nextLine();
+     
+    System.out.print("Digite a cor do Carro:");
+    carro2.cor = scan.nextLine();
+   
+    System.out.print("Digite o ano de fabricação do Carro:");
+    carro2.fabricacao = scan.nextLine();
+     
+    System.out.print("Digite a largura do Carro:");
+    carro2.largura = scan.nextLine();
+   
+    System.out.print("Digite o peso do Carro:");
+    carro2.peso = scan.nextLine();
+    
+    System.out.println(
+    );
+    
+    
+    System.out.println("Nome do Carro:" + carro2.nome);
+    System.out.println("Marca do Carro:" + carro2.marca);
+    System.out.println("Cor do Carro:" + carro2.cor);
+    System.out.println("Fabricação do Carro:" + carro2.fabricacao);
+    System.out.println("largura do Carro:" + carro2.largura);
+    System.out.println("Peso do Carro:" + carro2.peso);
+    
+    
+    
+    
+    
+    
+    Carro carro3 = new Carro();
+    
+    System.out.print("Digite o nome do Carro:");
+    carro3.nome = scan.nextLine();
+    
+    System.out.print("Digite a marca do Carro:");
+    carro3.marca = scan.nextLine();
+   
+    System.out.print("Digite a cor do Carro:");
+    carro3.cor = scan.nextLine();
+   
+    System.out.print("Digite o ano de fabricação do Carro:");
+    carro3.fabricacao = scan.nextLine();
+    
+    System.out.print("Digite a largura do Carro:");
+    carro3.largura = scan.nextLine();
+    
+    System.out.print("Digite o peso do Carro:");
+    carro3.peso = scan.nextLine();
+    
+     System.out.println("Nome do Carro:" + carro3.nome);
+     System.out.println("Marca do Carro:" + carro3.marca);
+     System.out.println("Cor do Carro:" + carro3.cor);
+     System.out.println("Fabricação do Carro:" + carro3.fabricacao);
+     System.out.println("largura do Carro:" + carro3.largura);
+     System.out.println("Peso do Carro:" + carro3.peso);
+   
+    }   
+}
